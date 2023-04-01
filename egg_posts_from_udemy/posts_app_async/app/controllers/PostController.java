6bea34b5fbf522e39094487be7aacfce7fe15ca1
Michael Easter <codetojoy@gmail.com>
2023-04-01 18:21:50 -0300

    async is OK

--- egg_posts_from_udemy/posts_app_async/app/controllers/PostController.java
+++ egg_posts_from_udemy/posts_app_async/app/controllers/PostController.java
@@ -45,50 +45,31 @@ public class PostController extends Controller {
         if (postId == null) {
             return postService.getPosts().thenApplyAsync(posts -> ok(views.html.posts.render(posts, form, request, messagesApi.preferred(request))), ec.current());
         } else {
-            return null;
-            /*
             return postService.findPost(postId).thenApplyAsync(post -> {
+               System.out.println("TRACER PostController index cp 1. postId: " + postId);
                 List<Post> posts = List.of();
-                if (post != null) {
-                    posts = List.of(post);
+                if (post.isPresent()) {
+                    posts = List.of(post.get());
                 }
-                return ok(views.html.posts.render(posts, form, request, messagesApi.preferred(request)), ec.current());
-            });
-            */
+                return ok(views.html.posts.render(posts, form, request, messagesApi.preferred(request)));
+            }, ec.current());
         }
     }
 
     public CompletionStage<Result> createPost(Http.Request request) {
-        return null;
-    }
-/*
         Form<PostForm> boundForm = form.bindFromRequest(request);
         if (boundForm.hasErrors()) {
-            postService.getPosts().thenApplyAsync(posts -> {
+            return postService.getPosts().thenApplyAsync(posts -> {
                 return badRequest(views.html.posts.render(posts, boundForm, request, messagesApi.preferred(request)));
-            });
+            }, ec.current());
         } else {
             MyLog.log("controller createPost cp 0");
             PostForm data = boundForm.get();
-            return postService.addPost(data).thenApplyAsync( -> {
+            return postService.addPost(data).thenApplyAsync(post -> {
                 MyLog.log("controller createPost cp 1");
                 System.out.println("TRACER added new post with title: " + data.getTitle());
                 return redirect(routes.PostController.index(null)).flashing("info", "TRACER Post added!");
-            });
-        }
-        /*
-
-        if (boundForm.hasErrors()) {
-            System.err.println("TRACER post errors: " + boundForm.errors());
-            return badRequest(views.html.posts.render(postService.getPosts(), boundForm, request, messagesApi.preferred(request)));
-        } else {
-            MyLog.log("controller createPost cp 0");
-            PostForm data = boundForm.get();
-            postService.addPost(data);
-            System.out.println("TRACER added new post with title: " + data.getTitle());
-            return redirect(routes.PostController.index(null)).flashing("info", "TRACER Post added!");
+            }, ec.current());
         }
     }
-*/
 }
-
