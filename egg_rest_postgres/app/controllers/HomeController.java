6165642c20cc968dc8ff21bee334cf09a4bd4ecb
Michael Easter <codetojoy@gmail.com>
2023-04-09 15:22:03 -0300

    use Environment for mode

--- egg_rest_postgres/app/controllers/HomeController.java
+++ egg_rest_postgres/app/controllers/HomeController.java
@@ -1,15 +1,24 @@
 package controllers;
 
+import javax.inject.Inject;
+
 import play.mvc.*;
+import play.api.Environment;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+   
+    private Environment env;
 
-    public Result index() {
-        return ok(views.html.index.render());
+    @Inject
+    public HomeController(Environment env) {
+        this.env = env;
     }
 
+    public Result index() {
+        return ok(views.html.index.render(env));
+    }
 }
