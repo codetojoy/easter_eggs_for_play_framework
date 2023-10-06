bfa2b0d66b7a4f2ea6244fb5fd2ca1ecf5543517
Michael Easter <codetojoy@gmail.com>
2023-10-06 06:23:44 -0300

    new egg for hcaptcha

---
+++ egg_hcaptcha/app/models/Bar.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Bar {
+    public String id;
+    public String name;
+
+    public Bar(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ egg_hcaptcha/app/models/Foo.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Foo {
+    public String firstName;
+    public String lastName;
+
+    public Foo(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
